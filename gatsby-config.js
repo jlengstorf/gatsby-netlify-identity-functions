521ecab2dc344b3493002477c17f5fc053391309
Jason Lengstorf <jason@lengstorf.com>
2019-05-23 09:58:06 -0700

    chore: upgrade deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-compile-es6-packages",
+      options: {
+        modules: ["react-netlify-identity-widget"],
+      },
+    },
   ],
 }
